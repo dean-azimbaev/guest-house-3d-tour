977b953c5d8851a745e49aad29e321790c028f37
dazimbaev <dean.azimbaev03@gmail.com>
2024-07-10 22:09:35 +0600

    initial

---
+++ data.js
@@ -0,0 +1,571 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "Пройти во двор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.609496605479862,
+          "pitch": 0.19228239015937199,
+          "rotation": 0,
+          "target": "4--bbq"
+        },
+        {
+          "yaw": -2.786613710663289,
+          "pitch": 0.13028081073923303,
+          "rotation": 0,
+          "target": "1---"
+        },
+        {
+          "yaw": -0.15085009046206466,
+          "pitch": 0.18540921781357156,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": 0.5060366586475986,
+          "pitch": 0.2644778717318115,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---",
+      "name": "Выйти на улицу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.2560863917520404,
+        "pitch": 0.14341929194725367,
+        "fov": 1.560052767881924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15067163285806373,
+          "pitch": 0.5479736732144822,
+          "rotation": 12.566370614359176,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "Вход в юрту",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2049215280718037,
+          "pitch": 0.5585216888527107,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -0.25191396415040046,
+          "pitch": 0.4349539041175081,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Юрта",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.80557002552783,
+          "pitch": 0.14640031289693134,
+          "rotation": 0,
+          "target": "2---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--bbq",
+      "name": "Зона BBQ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.320953966719678,
+          "pitch": 0.418542328496077,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9769622086513383,
+          "pitch": -0.01327984034804075,
+          "title": "Мангал",
+          "text": "В Гостевом доме \"Адилет\" есть мангал, на котором можно приготовить шашлык, стейки или пожарить крылышки на сетке"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "Двор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9160460712388137,
+          "pitch": 0.21641360075012983,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -1.419534022398734,
+          "pitch": 0.23300889294224625,
+          "rotation": 0,
+          "target": "2---"
+        },
+        {
+          "yaw": -0.32975450539451856,
+          "pitch": 0.13629261113941027,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.5248226140798344,
+          "pitch": 0.1320280010473578,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 2.850671562238306,
+          "pitch": 0.12729605963813917,
+          "rotation": 0,
+          "target": "4--bbq"
+        },
+        {
+          "yaw": 0.06333602272899519,
+          "pitch": 0.149278734064751,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "Администрация",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.134771741218536,
+          "pitch": 0.21863584602972885,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.212610864276547,
+          "pitch": 0.06992826447969414,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "Бассейн",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8257855124180171,
+          "pitch": 0.05483841274730139,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 3.066198334795401,
+          "pitch": 0.10553583753640439,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "Шезлонги",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8970518825428293,
+          "pitch": 0.03041829357396253,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 1.4654824905962016,
+          "pitch": 0.13489688709630343,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.5647617232626398,
+          "pitch": -0.4202987732902308,
+          "rotation": 0,
+          "target": "10----3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "Тапчан",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7063983493584534,
+          "pitch": 0.17171450307845326,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10----3-",
+      "name": "Панорманый вид с 3 этажа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2456952326317392,
+          "pitch": 0.697170940836962,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.3747584619413633,
+          "pitch": 0.4001577682588948,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": 1.8887978608971263,
+          "pitch": 0.5927850044338605,
+          "rotation": 0,
+          "target": "11---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11---",
+      "name": "Вход в бассейн",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1820716301683536,
+          "pitch": 0.12687553839695376,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -0.6079115160701107,
+          "pitch": 0.07039179480407753,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Adilet guest house",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
